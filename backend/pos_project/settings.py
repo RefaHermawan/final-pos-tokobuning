efe4c482174a255440b46c12ad4d9a365274c556
Refa <refahermawan27@gmail.com>
2025-12-24 13:58:19 +0700

    adjust database

--- backend/pos_project/settings.py
+++ backend/pos_project/settings.py
@@ -89,27 +89,27 @@ WSGI_APPLICATION = 'pos_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DATABASE_NAME'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': os.environ.get('DATABASE_HOST'),
-#         'PORT': os.environ.get('DATABASE_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sembako_db',        # Nama database yang Anda buat
-        'USER': 'postgres',      # User yang Anda buat
-        'PASSWORD': 'portgasdace123', # Password yang Anda set
-        'HOST': 'localhost',     # Atau IP server database Anda
-        'PORT': '5432',          # Port default PostgreSQL
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sembako_db',        # Nama database yang Anda buat
+#         'USER': 'postgres',      # User yang Anda buat
+#         'PASSWORD': 'portgasdace123', # Password yang Anda set
+#         'HOST': 'localhost',     # Atau IP server database Anda
+#         'PORT': '5432',          # Port default PostgreSQL
+#     }
+# }
 
 database_url = os.environ.get('DATABASE_URL')
 if database_url:
